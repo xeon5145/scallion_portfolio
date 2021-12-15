78834e2d842440b963b5c33668bbb3ce7bc7edc8
Abhishek Kagra <abhisheksingh5145@gmail.com>
2021-12-15 14:24:26 +0530

    set up mailer to send mail from deathvalerians@gmail.com by name Onion Portfolio

--- include/mailer.php
+++ include/mailer.php
@@ -7,4 +7,30 @@ require "phpMailer/Exception.php";
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+
+//create instance
+$mail = new PHPMailer();
+//set mailer
+$mail->isSMTP();
+//HTML true
+$mail->isHTML(true); 
+//define SMTP host
+$mail->Host = "smtp.gmail.com";
+//enable smtp auth
+$mail->SMTPAuth = "true";
+//set type of encryption (ssl/tls)
+$mail->SMTPSecure = "tls";
+//set port to connect smtp
+$mail->Port = "587";
+//set gmail username
+$mail->Username = "deathvalerians@gmail.com";
+//set gmail password
+$mail->Password = "AOBFNTNDR";
+//set sender mail
+$mail->setFrom("deathvalerians@gmail.com", "Onion Portfolio");
+
+// subject
+// body
+// address
+// to be set on server side
  ?>
