cd5bb2c2875a18adc0efdc2230d942bd1ced8123
Abhishek Kagra <abhisheksingh5145@gmail.com>
2021-12-16 18:39:51 +0530

    added bootstrap 5 css cdn link

---
+++ include/main_header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- ================================== bootstrap 5 ================================== -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- ================================== bootstrap 5 ================================== -->
+    <title></title>
+  </head>
+  <body>
