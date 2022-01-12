7780878fb630fd8c664f64fb9815d0efaeddf628
Abhishek Kagra <abhisheksingh5145@gmail.com>
2022-01-12 20:03:46 +0530

    removed bootstrap 5 files , now i will use pure css

--- html/index.php
+++ html/index.php
@@ -1,17 +1,17 @@
-<div class="container login_form bg-primary text-white ">
-<div class="head-text text-center h4 p-3">
+<div class="login_form">
+<div class="">
   <span>Login</span>
 </div>
       <form class="" method="post">
           <label for="username">Username / Email</label><br/>
-          <input class="w-75" type="text" id="username" name="username" placeholder="Enter Your Username or Email"><br/>
+          <input class="" type="text" id="username" name="username" placeholder="Enter Your Username or Email"><br/>
           <label for="password">Password</label><br/>
-          <input class="w-75" type="password" name="password" placeholder="Enter Your Password"><br/>
-          <a class="text-white link_login_form" href="create_account.php">Create Account</a><br/>
-          <a class="text-white link_login_form" href="forgot_password.php">Forgot Password</a><br/>
+          <input class="" type="password" name="password" placeholder="Enter Your Password"><br/>
+          <a class="link_login_form" href="create_account.php">Create Account</a><br/>
+          <a class="link_login_form" href="forgot_password.php">Forgot Password</a><br/>
           <?php
             echo $login_status;
           ?>
-        <input class="btn btn-primary" type="submit" name="login" value="Login">
+        <input class="login_button" type="submit" name="login" value="Login">
       </form>
     </div>
--- include/main_footer.php
+++ include/main_footer.php
@@ -1,6 +1,6 @@
-<footer class="container-fluid p-3 fixed-bottom main_footer bg-primary text-white">
-    <p class="h6">Home</p>
-    <p class="h6">About Us</p>
+<footer class="">
+    <p class="">Home</p>
+    <p class="">About Us</p>
 </footer>
 </body>
 </html>
