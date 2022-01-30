5e179b573d6c2c32dba4ce75a54ea1626b85c62e
Abhishek Kagra <abhisheksingh5145@gmail.com>
2022-01-30 18:35:55 +0530

    added html part of dashboard

--- html/dashboard.php
+++ html/dashboard.php
@@ -4,9 +4,17 @@ part - 1 is about the card
 which contains the for which takes the inputs of the crypto that is in hold position
 part 1 ends
 -->
-<div class="container">
+<div class="">
+  <!-- welcome card -->
+  <p>Hi , Abhishek</p>
+  <p>Total Profit / Loss : </p>
+  <p>Owned Cryptos : </p>
+</div>
+
+<div class="">
+  <!-- add watchlist form card -->
   <p>register crypto</p>
-  <div class="form-control">
+  <div class="">
     <form class="" method="post">
       <div class="">
 
@@ -28,4 +36,30 @@ part 1 ends
     </div>
     </form>
   </div>
+</div>
+
+<div class="">
+  <!-- watchlist table card -->
+  <table border="collapse">
+    <thead>
+      <th>Name</th>
+      <th>Current Price</th>
+      <th>Buy Price</th>
+      <th>Quantity</th>
+      <th>Profit / Loss</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Solana</td>
+        <td>7,568</td>
+        <td>10,789</td>
+        <td>10</td>
+        <td>3,891</td>
+      </tr>
+    </tbody>
+    <tr>
+      <td colspan="4">Total Profit / Loss</td>
+      <td>2,347</td>
+    </tr>
+  </table>
 </div>
