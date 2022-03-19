099550861dd050a04512c07edaff3d1bbb842f04
Abhishek Kagra <abhisheksingh5145@gmail.com>
2022-03-19 20:43:24 +0530

    added logout functionality

--- include/account_header.php
+++ include/account_header.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="dist/css/dashboard.css">
-  </head>
-<body>
-  <header>
-    <div class="head_bar">
-      <span>Onion Portfolio</span>
-      <button type="button" name="logout">Logout</button>
-    </div>
-  </header>
+<?php
+
+if(isset($_POST['logout']))
+{
+  header('location: index.php');
+  unset($_SESSION['username']);
+}
+include "html/account_header.php";
+?>
---
+++ include/html/account_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/css/dashboard.css">
+  </head>
+<body>
+  <header>
+    <div class="head_bar">
+      <span>Scallian Portfolio</span>
+      <form method="post">
+      <input type="submit" name="logout" value="Logout">
+      </form>
+    </div>
+  </header>
