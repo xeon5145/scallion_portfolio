d2f7fd87315f7743c7cbd3df44a132dca557e156
Sahil Bhardwaj <binny4166@gmail.com>
2022-04-18 12:16:20 +0530

    create acc css

--- html/create_account.php
+++ html/create_account.php
@@ -1,14 +1,21 @@
-    <div class="">
+<div class="">
       <?php
       echo $account_message;
        ?>
+</div>
+<div class="create_account_box">
+   <div class="box_head">
+     <p>Create Account</p>
     </div>
+
+
 <form method="post">
-<input type="firstname" name="f_name" placeholder="Enter Firstname">
-<input type="lastname" name="l_name" placeholder="Enter Lastname">
-<input type="email" name="email" placeholder="Enter Email">
-<input type="username" name="u_name" placeholder="Choose Username">
-<input type="text" name="pwd_p" placeholder="Choose Password">
-<input type="password" name="pwd_c" placeholder="Confirm Password">
-<input type="submit" name="crt_accnt" value="Submit">
+<input type="firstname" name="f_name" placeholder="Enter Firstname"></br/>
+<input type="lastname" name="l_name" placeholder="Enter Lastname"></br/>
+<input type="email" name="email" placeholder="Enter Email"></br/>
+<input type="username" name="u_name" placeholder="Choose Username"></br/>
+<input type="text" name="pwd_p" placeholder="Choose Password"></br/>
+<input type="password" name="pwd_c" placeholder="Confirm Password"></br/>
+<input type="submit" name="crt_accnt" value="Submit"></br/>
 </form>
+</div>
